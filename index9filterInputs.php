1fa748367f9201e6464797c1e08145cbc9044119
karnVen <onemonth06@gmail.com>
2026-03-26 18:22:04 +0530

    feat: FILTERINPUTSFUNCTION

---
+++ index9filterInputs.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="index.php" method="post">
+
+    <label for="">username:</label>
+    <input type="text" name="username">
+    <label for="">age:</label>
+    <input type="text" name="age">
+    <input type="submit" name="login" value="login">
+    </form>
+</body>
+</html>
+
+<?php
+
+if(isset($_POST["login"])){
+   // $username = $_POST["username"]; <script>alert('You have been Hacked!');</script>  cant handle exicutable statements
+
+
+    $username = filter_input(INPUT_POST,"username",FILTER_SANITIZE_SPECIAL_CHARS);
+
+    $age = filter_input(INPUT_POST,"age",FILTER_SANITIZE_NUMBER_INT);
+
+    //
+    echo $username;
+    echo $age;
+}
+
+?>
